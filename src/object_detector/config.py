d97642d3cd269de1f6b4e042e4e0cc1523a91d22
christianfaccio <christianfaccio@outlook.it>
2025-07-16 12:34:42 +0200

    added correct detection model

---
+++ src/object_detector/config.py
@@ -0,0 +1,18 @@
+import torchvision.transforms as transforms
+
+# Includes data augmentation to make the model more robust.
+TRAIN_TRANSFORMS = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Transformations for INFERENCE (validation, testing, and the final pipeline).
+# No augmentation is used here to ensure consistent, real-world evaluation.
+INFERENCE_TRANSFORMS = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
